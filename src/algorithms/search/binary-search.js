a39a65cb2ed0464f6d392ef553e6f3bb932965ed
aa-wong <waaron.wong@gmail.com>
2020-02-19 07:48:42 -0500

    added binary search algorithm

--- src/algorithms/search/binary-search.js
+++ src/algorithms/search/binary-search.js
@@ -2,10 +2,12 @@
 
 function binarySearch(array, val) {
   // get mid point of array
-  const mid = array.length / 2;
+  const mid = Math.floor(array.length / 2);
 
-  if (val > array[mid]) return binarySearch(array.splice(mid - 1, array.length), val);
-  if (val < array[mid]) return binarySearch(array.splice(0, mid - 1), val);
+  if (array.length > 1) {
+    if (val > array[mid]) return binarySearch(array.splice(mid), val);
+    if (val < array[mid]) return binarySearch(array.splice(0, mid), val);
+  }
   if (val === array[mid]) return array[mid];
   return null;
 };
